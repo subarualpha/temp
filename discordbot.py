1ae0d7b2a74107ec174a948189005f10310db75a
subarualpha <90103087+subarualpha@users.noreply.github.com>
2021-09-05 02:15:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,7 +8,7 @@ bot = commands.Bot(command_prefix='help.')
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    error_msg = 'エラーメッセージ'
     await ctx.send(error_msg)
 
 
