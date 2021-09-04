23becf48ee1428fd904031fc26b30c256456c1d5
subarualpha <90103087+subarualpha@users.noreply.github.com>
2021-09-05 01:57:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,5 +15,17 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+reply_data = {}
+@bot.command()
+async def ori(ctx, word, reply):
+    reply_data[word] = reply
+    await ctx.send(f"{word} -> {reply}を登録しました")
+
+@bot.event
+async def on_message(message):
+    reply = reply_data.get(message.content)
+    if reply:
+        await message.channel.send(reply)
+
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
