62ee90e03acfc4f79c10244ad72355b54edbc062
subarualpha <90103087+subarualpha@users.noreply.github.com>
2021-09-05 02:07:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,17 +2,25 @@ from discord.ext import commands
 from os import getenv
 import traceback
 
-reply_data = {}
-@bot.command()
-async def ori(ctx, word, reply):
-    reply_data[word] = reply
-    await ctx.send(f"{word} -> {reply}を登録しました")
+bot = commands.Bot(command_prefix='?')
+
 
 @bot.event
-async def on_message(message):
-    reply = reply_data.get(message.content)
-    if reply:
-        await message.channel.send(reply)
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+@bot.command(name="久しぶり")
+async def hello(ctx):
+    await ctx.send(f"どうも、{ctx.message.author.name}さん！")
+
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
