7a5ef68f30d1d2e028e1d247b6f95f250de67344
subarualpha <90103087+subarualpha@users.noreply.github.com>
2021-09-05 02:56:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 from os import getenv
 import traceback
 
-bot = commands.Bot(command_prefix='help.')
+bot = commands.Bot(command_prefix='s.')
 
 
 @bot.event
@@ -17,32 +17,32 @@ async def ping(ctx):
     await ctx.send('pong')
 
 
-@bot.command(name="DDoS")
+@bot.command(name="help.DDoS")
 async def hello(ctx):
     await ctx.send(f"DDoS対策はTCPShieldを介してのIPv4接続となります。\nDDoS攻撃を受けた場合はそちらが対応してくれるので大丈夫ですが、\n万が一DDoSが直接来た場合、参加型を一時中断いたします")
 
 
-@bot.command(name="サーバーに接続できない")
+@bot.command(name="help.サーバーに接続できない")
 async def hello(ctx):
     await ctx.send(f"考えられる候補\n1.サーバーアドレスがあっているか、 #お知らせ を常時確認してください。\n2.サーバーが起動しているか、@Minecraft#5528 がオンラインになっているか確認してください。又はメンテナンスになっていないか確認してください\n3.サーバーのバージョンがあっていない。サーバーバージョンは1.17.1となっております(2021/9/5現在)\n4.非正規のランチャーを使っていないか、TLauncherなどの非正規クライアントの場合、サーバーに入ることはできませんのでご了承ください\nその他ご不明な点があればサポートからお問い合わせください")
 
 
-@bot.command(name="TLauncher")
+@bot.command(name="help.TLauncher")
 async def hello(ctx):
     await ctx.send(f"TLauncherは対応しておりません。ご了承ください")
 
 
-@bot.command(name="badlion")
+@bot.command(name="help.badlion")
 async def hello(ctx):
     await ctx.send(f"badlionは検証しておりませんが、一応参加はできると思います。ご不明な点がございましたらサポートへお問い合わせください")
 
 
-@bot.command(name="チート")
+@bot.command(name="help.チート")
 async def hello(ctx):
     await ctx.send(f"チートクライアントの使用は一切許されておりません。発見次第BANいたします。")
 
 
-@bot.command(name="サーバー")
+@bot.command(name="help.サーバー")
 async def hello(ctx):
     await ctx.send(f"サーバー情報\nSpigot1.17.1\n...")
 
