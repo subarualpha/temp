b9006710a1f799a4683c371bb2b3a1ce11291340
subarualpha <90103087+subarualpha@users.noreply.github.com>
2021-09-05 02:03:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,19 +2,6 @@ from discord.ext import commands
 from os import getenv
 import traceback
 
-bot = commands.Bot(command_prefix='?')
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
 reply_data = {}
 @bot.command()
 async def ori(ctx, word, reply):
